e8f2d7d34bd2c098019951bcf5068c749bd1382b
batuhansolmaz <batuso123@gmail.com>
2024-10-26 18:39:08 +0300

    Initial commit

---
+++ src/main/java/com/example/tourist_app/TouristAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.tourist_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouristAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TouristAppApplication.class, args);
+	}
+
+}
